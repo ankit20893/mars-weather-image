7b0b3c12ef877b0408b643f87f2d11e1d1559036
Ankit Singh <ankitsingh@devnagri.com>
2020-03-28 10:17:27 +0530

    add pagination
    Signed-off-by: Ankit Singh <ankitsingh@devnagri.com>

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/Weather.jpg" width="400" height="400">
+    <HelloWorld msg="Welcome to Image & Weather App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import { BContainer, LayoutPlugin } from 'bootstrap-vue'
+import { BPagination } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+
+Vue.component('b-container', BContainer)
+Vue.component('b-pagination', BPagination)
+Vue.use(LayoutPlugin)
+Vue.use(VueAxios, axios)
 
 export default {
   name: 'App',
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,24 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input type="search"  v-on:keyup="fetchImages" v-model="term">
+
+    <b-container class="bv-example-row">
+      <b-row v-for="row in formattedimages" v-bind:key="row">
+        <b-col cols v-for="image in row" v-bind:key="image">
+          <img :src="image.links[0].href" width="100" height="100">
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-pagination
+            :change="changePagination"
+            :input ="updatePage(currentPage)"
+            v-if="images.length"
+            v-model="currentPage"
+            :total-rows="images.length"
+            :per-page="perPage"
+            aria-controls="my-table"
+    ></b-pagination>
   </div>
 </template>
 
@@ -35,6 +27,43 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      perPage: 3,
+      currentPage: 1,
+      term: '',
+      imageEndPoint: 'https://images-api.nasa.gov/search',
+      images : {},
+      formattedimages : {},
+      links: {}
+    }
+  },
+  computed: {
+    //
+  },
+  methods: {
+    fetchImages: function(event) {
+      let term = event.target.value;
+      this.axios.get(this.imageEndPoint+'?q='+term).then((response) => {
+        this.images = response.data.collection.items;
+        this.links = response.data.collection.links;
+        this.formattedImages();
+      })
+    },
+    formattedImages: function() {
+      this.formattedimages = this.images.reduce((c, n, i) => {
+        if (i % 2 === 0) c.push([]);
+        c[c.length - 1].push(n);
+        return c;
+      }, []);
+    },
+    changePagination(page) {
+      console.log(page);
+    },
+    updatePage(page) {
+      console.log(page);
+    }
   }
 }
 </script>
